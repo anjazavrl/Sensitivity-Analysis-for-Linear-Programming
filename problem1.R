da93b6ccac490d8c98181241be1e2212172e51b6
Anja Zavrl <zavrl.anja@gmail.com>
2017-02-23 12:12:13 +0100

    analiza novega pogoja

--- problem1.R
+++ problem1.R
@@ -4,12 +4,12 @@ library(lpSolveAPI)
 library(ggplot2)
 
 #1. primer
-#funkcija, ki sprejme linearni program in vrne rešitev s pomocjo simpleksne metode
+#funkcija, ki sprejme linearni program in vrne resitev s pomocjo simpleksne metode
 problem1 <- function(a11 = 1, a12 = 1, a21 = 2, a22 = 1,
                     b1 = 8, b2 = 10, c1 = 5000, c2 = 3000) {
   sol <- simplex(a = c(c1, c2), A1 = matrix(c(a11, a21, a12, a22), ncol = 2),
                  b1 = c(b1, b2), maxi = TRUE)
-  c(value = sol$value, sol$soln) #zanima nas samo optimalna vrednost funkcije in rešitve
+  c(value = sol$value, sol$soln) #zanima nas samo optimalna vrednost funkcije in resitve
 }
 
 r <- problem1() 
@@ -19,13 +19,13 @@ r <- problem1()
 #sprememba koeficienta b1 = 8
 
 rb1 <- seq(4.5, 10.5, 0.01) #koeficient b1 spreminjamo na intervalu [4.5, 10.5] s korakom 0.05
-sb1 <- sapply(rb1, function(x) problem1(b1 = x)) #zaženemo funkcijo problem1, kjer se nam koeficient b1 spreminja
+sb1 <- sapply(rb1, function(x) problem1(b1 = x)) #zacenemo funkcijo problem1, kjer se nam koeficient b1 spreminja
 
 db11 = rbind(data.frame(b1 = rb1, var = "x1", val = sb1["x1",]),
-            data.frame(b1 = rb1, var = "x2", val = sb1["x2",])) #definirana tabela, da se na grafu lahko loci rešitve
+            data.frame(b1 = rb1, var = "x2", val = sb1["x2",])) #definirana tabela, da se na grafu lahko loci resitve
 db12 = data.frame(rb1, sb1["value.b",]) #tabela za graf optimalne vrednosti
 
-#Graf za rešitvi x in y
+#Graf za resitvi x in y
 grafb1xy <- ggplot() + 
   geom_point(data = db11, aes(x = b1, y = val, colour = var))+
   ggtitle("Vpliv koeficienta b1 na rešitvi x in y")+
@@ -55,7 +55,7 @@ db21 = rbind(data.frame(b2 = rb2, var = "x1", val = sb2["x1",]),
             data.frame(b2 = rb2, var = "x2", val = sb2["x2",]))
 db22 = data.frame(rb2, sb2["value.b",])
 
-#Graf za rešitvi x in y
+#Graf za resitvi x in y
 grafb2xy <- ggplot() + 
   geom_point(data = db21, aes(x = b2, y = val, colour = var))+
   ggtitle("Vpliv koeficienta b2 na rešitvi x in y")+
@@ -88,7 +88,7 @@ dc11 = rbind(data.frame(c1 = rc1, var = "x1", val = sc1["x1",]),
              data.frame(c1 = rc1, var = "x2", val = sc1["x2",]))
 dc12 = data.frame(rc1, sc1["value.b",])
 
-#Graf za rešitvi x in y 
+#Graf za resitvi x in y 
 grafc1xy <- ggplot() + 
   geom_point(data = dc11, aes(x = c1, y = val, colour = var))+
   ggtitle("Vpliv koeficienta c1 na rešitvi x in y")+
@@ -118,7 +118,7 @@ dc21 = rbind(data.frame(c2 = rc2, var = "x1", val = sc2["x1",]),
              data.frame(c2 = rc2, var = "x2", val = sc2["x2",]))
 dc22 = data.frame(rc2, sc2["value.b",])
 
-#Graf za rešitvi x in y
+#Graf za resitvi x in y
 grafc2xy <- ggplot() + 
   geom_point(data = dc21, aes(x = c2, y = val, colour = var))+
   ggtitle("Vpliv koeficienta c2 na rešitvi x in y")+
@@ -140,3 +140,17 @@ ggsave(filename="grafc2opt.pdf", plot=grafc2opt)
 
 
 
+
+#dodajanje novega pogoja 5x + 3y <= 20
+
+novpogoj <- function(a11 = 1, a12 = 1, a21 = 2, a22 = 1, a31 = 5, a32 = 3,
+                     b1 = 8, b2 = 10, b3 = 20, c1 = 5000, c2 = 3000) {
+  sol <- simplex(a = c(c1, c2), A1 = matrix(c(a11, a21, a12, a22, a31, a32), nrow = 3, byrow = TRUE),
+                 b1 = c(b1, b2, b3), maxi = TRUE)
+  c(value = sol$value, sol$soln) #zanima nas samo optimalna vrednost funkcije in resitve
+}
+
+np <- novpogoj() 
+
+
+#rešitev se spremeni iz x = 6, y = 2, p = 28000 na x = 4, y = 0, p = 20000
